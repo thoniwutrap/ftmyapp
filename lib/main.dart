d36a9b11a534c96f53d933bd2e70518828e56c50
Thoniwut Rapeepisarn <thoniwut.r.dev@gmail.com>
2020-01-04 17:55:17 +0700

    add nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/menu_page.dart';
+import 'package:myapp/thrid.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,13 +16,24 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Tony'),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/mainmenu':
+            return PageTransition(child: MainMenuScreen(), type: PageTransitionType.fade);
+            break;
+          case '/third':
+            return PageTransition(child: ThirdScreen(), type: PageTransitionType.fade);
+            break;
+          default:
+            return null;
+        }
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
 
   final String title;
 
@@ -28,136 +43,145 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-bool monVal = false;
+  bool monVal = false;
 
   @override
   Widget build(BuildContext context) {
-
-
-        return Scaffold(
-          
-          body: Container(
-            color: Colors.blue,
-            child: Center(
+    return Scaffold(
+        body: Container(
+      color: Colors.blue,
+      child: Center(
           child: ListView(
-                  shrinkWrap: true,
-                     padding: const EdgeInsets.only(left: 200,right: 200),
+        shrinkWrap: true,
+        padding: const EdgeInsets.only(left: 100, right: 100),
+        children: <Widget>[
+          SizedBox(
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          fontFamily: 'Kanit'),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 10, bottom: 10, left: 50, right: 50),
+                      child: Text(
+                        "Email address",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Kanit'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 50, right: 50),
+                    child: new TextFormField(
+                      style: TextStyle(fontFamily: 'Kanit'),
+                      keyboardType: TextInputType.emailAddress,
+                      autofocus: false,
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 10, bottom: 10, left: 50, right: 50),
+                      child: Text(
+                        "Password",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Kanit'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 50, right: 50),
+                    child: new TextFormField(
+                      style: TextStyle(fontFamily: 'Kanit'),
+                      keyboardType: TextInputType.emailAddress,
+                      autofocus: false,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Row(
                       children: <Widget>[
-                        SizedBox(
-                          
-                                                  child: Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text('Sign In',style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 30,
-                                    fontFamily: 'Kanit'
-                                  ),),
-                                ),
-                                Container(
-                                  width: double.infinity,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 10,bottom: 10,left: 50,right: 50),
-                                    child: Text("Email address",style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Kanit'
-                                    ),
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 10,bottom: 10,left: 50,right: 50),
-                                  child: new TextFormField(
-                                    style: TextStyle(
-                                      fontFamily: 'Kanit'
-                                    ),
-                          keyboardType: TextInputType.emailAddress,
-                          autofocus: false,
-                          decoration: InputDecoration(
-                            hintText: 'Email',
-                            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                            border:
-                                  OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
-                          ),
-            ),
-                                ),
-      Container(
-                                  width: double.infinity,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 10,bottom: 10,left: 50,right: 50),
-                                    child: Text("Password",style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Kanit'
-                                    ),
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 10,bottom: 10,left: 50,right: 50),
-                                  child: new TextFormField(
-                                    style: TextStyle(
-                                      fontFamily: 'Kanit'
-                                    ),
-                          keyboardType: TextInputType.emailAddress,
-                          autofocus: false,
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                            border:
-                                  OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
-                          ),
-            ),
-                                ),
-                                 Padding(
-                                   padding: const EdgeInsets.only(left: 35),
-                                   child: Row(
-                                     children: <Widget>[
-                                       Checkbox(
+                        Checkbox(
                           value: monVal,
                           onChanged: (bool value) {
-                          setState(() {
-                            print(value);
-                                monVal = value;
-                          });
+                            setState(() {
+                              print(value);
+                              monVal = value;
+                            });
                           },
-        ),
-        Text('Remember me',style: TextStyle(
-            fontFamily: 'Kanit',
-            color: Colors.black
-        ),)
-                                     ],
-                                   ),
-                                 ),
-                                 Padding(
-                                   padding: EdgeInsets.only(left: 50,right: 50,bottom: 30),
-                                                          child: SizedBox(
-                                      
-                                       width: double.infinity,
-                                                            child: RaisedButton(
-                              
-                                       color: Colors.blue,
-                                       textColor: Colors.white,
-                                       child: Text('Sign In'),
-                                       onPressed: (){
-                                       },
-                                     ),
-                                   ),
-                                 )
-    ],
-            ),
-                          ),
                         ),
+                        Text(
+                          'Remember me',
+                          style: TextStyle(
+                              fontFamily: 'Kanit', color: Colors.black),
+                        )
                       ],
-          )
-        ),
-      )// This trailing comma makes auto-formatting nicer for build methods.
-    );
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 50, right: 50, bottom: 30),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text('Sign In'),
+                        onPressed: () {
+                          print("dsf");
+                          _incrementName();
+                          print( MediaQuery.of(context).size.width);
+                          Navigator.pushNamed(context, '/mainmenu');
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      )),
+    ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
+}
+
+_incrementName() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('teststr', "Thoniwut");
 }
